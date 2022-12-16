3470357f0a731bf44a1b7a8336f01dd4d128de78
Emin Saygı <emin_0001@hotmail.com>
2022-12-17 00:27:58 +0300

    GameListVM added

---
+++ GameZone/GameZone/ViewModel/GameViewModel.swift
@@ -0,0 +1,60 @@
+import Foundation
+
+
+
+
+import Foundation
+import UIKit
+
+struct GameListViewModel {
+    let results : [Game]
+    
+}
+extension GameListViewModel {
+    var numberOfSections: Int {
+        return results.count
+    }
+    
+    func numberOfRowsInSeciton(_ section:Int) ->Int {
+        return results.count
+    }
+    func gameAtIndex(_ index:Int) -> GameViewModel {
+        let game = self.results[index]
+        return GameViewModel(game)
+    }
+}
+
+
+
+
+
+
+
+//MARK: - 1.KISIM
+struct GameViewModel {
+    private let game: Game
+    
+}
+extension GameViewModel {
+    init(_ game: Game){
+        self.game = game
+    }
+}
+
+extension GameViewModel {
+    var name : String? {
+        return self.game.name
+    }
+    var rating : Double? {
+        return self.game.rating
+    }
+    var released : String? {
+        return self.game.released
+    }
+    var background_image : String? {
+        return self.game.background_image
+    }
+    
+}
+
+
