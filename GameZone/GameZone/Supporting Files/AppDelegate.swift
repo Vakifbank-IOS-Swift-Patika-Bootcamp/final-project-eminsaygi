17dcd5d787f709a1c95c3a764a6348a8912c1f00
Emin Saygı <emin_0001@hotmail.com>
2022-12-17 00:24:15 +0300

    Delagate files edited

--- GameZone/GameZone/Supporting Files/AppDelegate.swift
+++ GameZone/GameZone/Supporting Files/AppDelegate.swift
@@ -8,15 +8,11 @@
 import UIKit
 import CoreData
 
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
 
     // MARK: UISceneSession Lifecycle
 
@@ -41,7 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "GameZone")
+        let container = NSPersistentContainer(name: "GamesData")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
